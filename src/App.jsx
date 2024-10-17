b55a27914088e3e4253bf81c15bc31be3fd4b784
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-10-16 22:25:39 -0400

    files

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import './App.css'
 
 function App() {
 
+  function handleClick(){
+    alert('btn clicked')
+  }
+
+  const addFive = (num) => {
+    alert(num + 5);
+  }
   return (
     <>
-      <h1>Vite + React</h1>
+      <h2>React Core Concepts- part 2</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => alert('button2 clicked')}>Click Me2</button>
+      {/* we cannot write <button onClick={addFive(3)}>Click Me3</button> */}
+      <button onClick={() => addFive(3)}>Click Me3</button>
+
     </>
   )
 }
